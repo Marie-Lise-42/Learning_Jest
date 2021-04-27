d33611b463891dfe54caa1ffb87e432e0dce57a7
ML <marie.lise.92300@gmail.com>
2021-04-27 15:26:46 +0200

    first http post test

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,1 +1,6 @@
-exports.createTodo = () => {};
+const TodoModel = require("../model/todo.model");
+
+exports.createTodo = (req, res, next) => {
+    const createdModel = TodoModel.create(req.body);
+    res.status(201).json(createdModel);
+};
