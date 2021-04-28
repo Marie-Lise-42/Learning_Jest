9776b6f4a32126009683f7916a27968aecb49d8e
ML <marie.lise.92300@gmail.com>
2021-04-28 15:22:37 +0200

    get route without error handling

--- app.js
+++ app.js
@@ -13,6 +13,4 @@ app.get("/", (req, res) => {
     res.json("Hello the world!");
 });
 
-
-
 module.exports = app;
--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -4,3 +4,8 @@ exports.createTodo = async (req, res, next) => {
     const createdModel = await TodoModel.create(req.body);
     res.status(201).json(createdModel);
 };
+
+exports.getTodos = async (req, res, next) => {
+    const allTodos = await TodoModel.find({});
+    res.status(200).json(allTodos);
+};
